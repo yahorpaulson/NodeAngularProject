3b199f4a36eece42671d40d4939be5149ecfac54
yahorpaulson <y.paulson@yahoo.com>
2024-02-05 13:16:06 +0100

    [4] included packages for parsing post requests, working with othe r domains and logging requests through terminal

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth')
+
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
@@ -7,7 +9,15 @@ const positionRoutes = require('./routes/position')
 
 const app = express();
 
+
+
 //concat
+
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(require('cors')())
+
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
 app.use('/api/category', categoryRoutes)
