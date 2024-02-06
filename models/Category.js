c2283de9bd473a5b9688c84cc47ccf77d477d96e
yahorpaulson <y.paulson@yahoo.com>
2024-02-06 14:33:27 +0100

    [5]created models with mongoose for database

---
+++ models/Category.js
@@ -0,0 +1,17 @@
+const mongoose = requre('mongoose')
+const Schema = mongoose.Schema
+
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    imageSrc: {
+        type: String,
+        default: ''
+    },
+    user: {
+        ref: 'users',
+        type: Schema.Types.ObjectId
+    }
+})
