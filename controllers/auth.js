4ad86b433baab7f679e8d41b92ad1bcfced54062
yahorpaulson <y.paulson@yahoo.com>
2024-02-08 22:48:59 +0100

    [9] user registration with password encryption

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 
 
@@ -22,5 +23,20 @@ module.exports.register = async function(req, res) {
         })
     } else{
         //create user
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+
+        try{
+            await user.save()
+            res.status(201).json(user)
+        } catch(e){
+            //error
+            
+        }
+        
     }
 }
