9d84683148efde8f76de563d5112c50dc42bb5a4
yahorpaulson <y.paulson@yahoo.com>
2024-02-16 10:52:04 +0100

    [17] materialize theme was set up

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,5 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import 'materialize-css/dist/js/materialize.js'
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
